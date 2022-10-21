b8399f6f5be7d13e3e90ce55fe544941fb66c9f0
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-10-21 16:25:04 -0400

    Revert "Updates!"
    This reverts commit 71266650fd475e762ab38fbfb6b60f527a2037ca.

--- CMake/Jsonifier.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-fetchcontent_declare(
-	Jsonifier
-	URL https://github.com/RealTimeChris/Jsonifier/releases/download/Master/Jsonifier-src.zip
-)
-
-fetchcontent_makeavailable(Jsonifier)
