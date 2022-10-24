4e6ab954c837500d0e04ddc09ae2e959f84ee488
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-10-24 04:29:31 -0400

    Updates!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <source_location>
 #include <rapidjson/rapidjson.h>
 #include <immintrin.h>
+#include <simdjson.h>
 
 struct UpdatePresenceDataTwo {
 	std::string status{};///< Current status.
@@ -107,7 +108,7 @@ WebSocketIdentifyData::operator Jsonifier::Jsonifier() {
 	serializer["op"] = 2;
 	return serializer;
 }
-
+/*
 int32_t main() noexcept {
 	try {
 
@@ -169,4 +170,101 @@ int32_t main() noexcept {
 
 	return 0;
 }
+*/
+#include <immintrin.h>//SSE Extensions
 
+
+const int N = 1024*1024;//Number of tests
+const int V = N / 64;//Vectorized size
+
+
+using namespace std;
+using namespace std::chrono;
+
+high_resolution_clock::time_point now = high_resolution_clock::now();
+#define TIME duration_cast<duration<double>>(high_resolution_clock::now() - now).count();
+
+class MTString {
+  public:
+	using AllocatorType = std::allocator<char>;
+	using AllocatorTraits = std::allocator_traits<AllocatorType>;
+	using SizeType = size_t;
+	operator std::string();
+	MTString& operator+=(const std::string&) noexcept;
+	MTString(SizeType size) noexcept;
+	~MTString() noexcept;
+  protected:
+	alignas(512) char* string{ nullptr };
+	alignas(512) SizeType currentSize{};
+	alignas(512) std::string stringReal{};
+	alignas(512) std::unique_ptr<__m512i[]> intrinsics{};
+
+};
+
+MTString& MTString::operator+=(const std::string & other) noexcept {
+	if (this->stringReal.size() < other.size()) {
+		this->stringReal.resize(other.size());
+	}
+	if (this->currentSize < other.size() / 64) {
+		this->intrinsics = std::make_unique<__m512i[]>(other.size() / 64);
+		this->currentSize = other.size() / 64;
+	}
+	for (int64_t x = 0; x < other.size() / 64; ++x) {
+		intrinsics[x] = _mm512_load_si512(( __m512i* )(other.data() + (x * 64)));
+	}
+	for (int64_t x = 0; x < other.size() / 64; ++x) {
+		_mm512_store_si512(( __m512i* )(this->stringReal.data() + (x * 64)), intrinsics[x]);
+	}
+	return *this;
+}
+
+MTString::MTString(SizeType size) noexcept {
+}
+
+MTString::~MTString() noexcept {
+}
+
+MTString::operator std::string() {
+	return std::move(this->stringReal);
+}
+
+int main() {
+
+	now = high_resolution_clock::now();
+	std::vector<char> alignas(64) theVectorOne{};
+	std::string alignas(64) stringOne{};
+	stringOne.resize(1024*1024); 
+	for (uint64_t x = 0; x < 1024 * 1024; ++x) {
+		theVectorOne.push_back(x);
+	}
+	for (uint64_t x = 0; x < 1024 * 1024; ++x) {
+		stringOne[x] = x;
+	}
+	MTString newString{ 1024 * 1024 };
+	now = high_resolution_clock::now();
+	for (int i = 0; i < 20; ++i) {
+		newString += stringOne;
+	}
+	double avx_time = TIME;
+	cerr << "AVX sqrtf : " << avx_time << endl;
+	//std::cout << newString.operator std::string() << std::endl;
+
+	now = high_resolution_clock::now();
+	std::vector<char> theVector{};
+	std::string string{};
+	for (uint64_t x = 0; x < 1024 * 1024; ++x) {
+		string.push_back(x);
+	}
+	std::string newStringTwo{};
+	now = high_resolution_clock::now();
+	for (int i = 0; i < 20; ++i) {
+		newStringTwo = stringOne;
+	}
+		
+	double linear_time = TIME;
+	cerr << "Normal sqrtf: " << linear_time << endl;
+	
+	cout << "CG> message -channel \"exercise results\" Linear to AVX improvement : " << (linear_time / avx_time * 100) << "%" << endl;
+
+	return 0;
+}
