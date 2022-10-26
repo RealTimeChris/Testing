10f48c9525342ec87caa8a955443431e95274bdb
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-10-26 14:55:36 -0400

    Updates!

--- CMake/Jsonifier.cmake
+++ CMake/Jsonifier.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 fetchcontent_declare(
 	Jsonifier
 	URL https://github.com/RealTimeChris/Jsonifier/releases/download/Master/Jsonifier-src.zip
-	GIT_TAG cbd26d7efe963f59829cd5bcaa9d6740ddd5daec
+	GIT_TAG 5421d1fecd48475079c0363d9b5d10597d1da493
 )
 
 fetchcontent_makeavailable(Jsonifier)
