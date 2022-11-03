16ebc8f4786a9d17e84f90da8099be3c764a3f20
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-11-02 21:26:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -222,7 +222,7 @@ int32_t main() noexcept {
 void printValueAsString(__m256i in,std::string values) {
 	alignas(16) uint8_t v[32];
 	_mm256_store_si256(( __m256i* )v, in);
-	printf(std::string{ values.c_str() + std::string{ "v32_u8: %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n" } }.c_str(),
+	printf(std::string{ values.c_str() + std::string{ " v32_u8: %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n" } }.c_str(),
 		v[24], v[25], v[26], v[27], v[28], v[29], v[30], v[31], v[16], v[17], v[18], v[19], v[20], v[21], v[22], v[23], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15], v[0],
 		v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
 }
