83a82809da58edbf9f062a5e3bbb413a545c24e9
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-11-05 14:13:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -54,17 +54,17 @@ template<typename OTy> struct SimdBase {
 };
 
 void printValueAsString(__m256i in,std::string values) {
-	printf(std::string{ values.c_str() + std::string{ " v32_u8: %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n" } }.c_str(), in.m256i_i8[31],
-		in.m256i_i8[30], in.m256i_i8[29], in.m256i_i8[28], in.m256i_i8[27], in.m256i_i8[26], in.m256i_i8[25], in.m256i_i8[24], in.m256i_i8[23], in.m256i_i8[22], in.m256i_i8[21],
-		in.m256i_i8[20], in.m256i_i8[19], in.m256i_i8[18], in.m256i_i8[17], in.m256i_i8[16], in.m256i_i8[15], in.m256i_i8[14], in.m256i_i8[13], in.m256i_i8[12], in.m256i_i8[11],
-		in.m256i_i8[10], in.m256i_i8[9], in.m256i_i8[8], in.m256i_i8[7], in.m256i_i8[6], in.m256i_i8[5], in.m256i_i8[4], in.m256i_i8[3], in.m256i_i8[2], in.m256i_i8[1],
-		in.m256i_i8[0]);
+	printf(std::string{ values.c_str() + std::string{ " v32_u8: %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n" } }.c_str(), in.m256i_i8[0],
+		in.m256i_i8[1], in.m256i_i8[2], in.m256i_i8[3], in.m256i_i8[4], in.m256i_i8[5], in.m256i_i8[6], in.m256i_i8[7], in.m256i_i8[8], in.m256i_i8[9], in.m256i_i8[10],
+		in.m256i_i8[11], in.m256i_i8[12], in.m256i_i8[13], in.m256i_i8[14], in.m256i_i8[15], in.m256i_i8[16], in.m256i_i8[17], in.m256i_i8[18], in.m256i_i8[19], in.m256i_i8[20],
+		in.m256i_i8[21], in.m256i_i8[22], in.m256i_i8[23], in.m256i_i8[24], in.m256i_i8[25], in.m256i_i8[26], in.m256i_i8[27], in.m256i_i8[28], in.m256i_i8[29], in.m256i_i8[30],
+		in.m256i_i8[31]);
 	printf(
 		std::string{ values.c_str() + std::string{ " (DIGITS) v32_u8: %d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d\n" } }.c_str(),
-		in.m256i_i8[31], in.m256i_i8[30], in.m256i_i8[29], in.m256i_i8[28], in.m256i_i8[27], in.m256i_i8[26], in.m256i_i8[25], in.m256i_i8[24], in.m256i_i8[23], in.m256i_i8[22],
-		in.m256i_i8[21], in.m256i_i8[20], in.m256i_i8[19], in.m256i_i8[18], in.m256i_i8[17], in.m256i_i8[16], in.m256i_i8[15], in.m256i_i8[14], in.m256i_i8[13], in.m256i_i8[12],
-		in.m256i_i8[11], in.m256i_i8[10], in.m256i_i8[9], in.m256i_i8[8], in.m256i_i8[7], in.m256i_i8[6], in.m256i_i8[5], in.m256i_i8[4], in.m256i_i8[3], in.m256i_i8[2],
-		in.m256i_i8[1], in.m256i_i8[0]);
+		in.m256i_i8[0], in.m256i_i8[1], in.m256i_i8[2], in.m256i_i8[3], in.m256i_i8[4], in.m256i_i8[5], in.m256i_i8[6], in.m256i_i8[7], in.m256i_i8[8], in.m256i_i8[9],
+		in.m256i_i8[10], in.m256i_i8[11], in.m256i_i8[12], in.m256i_i8[13], in.m256i_i8[14], in.m256i_i8[15], in.m256i_i8[16], in.m256i_i8[17], in.m256i_i8[18], in.m256i_i8[19],
+		in.m256i_i8[20], in.m256i_i8[21], in.m256i_i8[22], in.m256i_i8[23], in.m256i_i8[24], in.m256i_i8[25], in.m256i_i8[26], in.m256i_i8[27], in.m256i_i8[28], in.m256i_i8[29],
+		in.m256i_i8[30], in.m256i_i8[31]);
 }
 
 void printValueAsString(uint64_t inA, std::string values) {
@@ -74,18 +74,21 @@ void printValueAsString(uint64_t inA, std::string values) {
 			v[x] = 1;
 		}
 	}
-	printf(std::string{ values.c_str() + std::string{ " v64_u8: %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n" } }.c_str(), v[63], v[62], v[61], v[60],
-		v[59], v[58], v[57], v[56], v[55], v[54], v[53], v[52], v[51], v[50], v[49], v[48], v[47], v[46], v[45], v[44], v[43], v[42], v[41], v[40], v[39], v[38], v[37], v[36],
-		v[35], v[34], v[33], v[32], v[31], v[30], v[29], v[28], v[27], v[26], v[25], v[24], v[23], v[22], v[21], v[20], v[19], v[18], v[17], v[16], v[15], v[14], v[13], v[12],
-		v[11], v[10], v[9], v[8], v[7], v[6], v[5], v[4], v[3], v[2], v[1], v[0]);
+	printf(std::string{ values.c_str() +
+			   std::string{
+				   " v64_u8: %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n" } }
+			   .c_str(),
+		v[32], v[33], v[34], v[35], v[36], v[37], v[38], v[39], v[40], v[41], v[42], v[43], v[44], v[45], v[46], v[47], v[48], v[49], v[50], v[51], v[52], v[53], v[54], v[55],
+		v[56], v[57], v[58], v[59], v[60], v[61], v[62], v[63], v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15], v[16], v[17],
+		v[18], v[19], v[20], v[21], v[22], v[23], v[24], v[25], v[26], v[27], v[28], v[29], v[30], v[31]);
 	printf(std::string{ values.c_str() +
 			   std::string{ " (DIGITS) v64_u8: "
-							"%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d\n" } }
+							"%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'"
+							"%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d'%d\n" } }
 			   .c_str(),
-		v[63], v[62], v[61], v[60], v[59], v[58], v[57], v[56], v[55], v[54], v[53], v[52], v[51], v[50], v[49], v[48], v[47], v[46], v[45], v[44], v[43], v[42], v[41], v[40], v[39],
-		v[38], v[37], v[36], v[35], v[34], v[33], v[32] ,v[31], v[30], v[29], v[28], v[27], v[26], v[25], v[24], v[23], v[22], v[21], v[20], v[19], v[18], v[17], v[16], v[15], v[14],
-		v[13], v[12], v[11], v[10], v[9], v[8], v[7],
-		v[6], v[5], v[4], v[3], v[2], v[1], v[0]);
+		v[32], v[33], v[34], v[35], v[36], v[37], v[38], v[39], v[40], v[41], v[42], v[43], v[44], v[45], v[46], v[47], v[48], v[49], v[50], v[51], v[52], v[53], v[54], v[55],
+		v[56], v[57], v[58], v[59], v[60], v[61], v[62], v[63], v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15], v[16], v[17],
+		v[18], v[19], v[20], v[21], v[22], v[23], v[24], v[25], v[26], v[27], v[28], v[29], v[30], v[31]);
 }
 
 void printValueAsString(uint32_t inA, std::string values) {
@@ -123,17 +126,24 @@ std::string reverseString(std::string inputString) {
 	}
 	return newString;
 }
-
+__m256i packStringIntoValue(const char* string) {
+	__m256i value{};
+	for (size_t x = 0; x < 32; ++x) {
+		value.m256i_i8[x] = string[x];
+	}
+	return value;
+}
 uint64_t collectCarries(uint64_t inputA, uint64_t inputB) {
 	uint64_t returnValue{};
-	_addcarry_u64(0, inputA, inputB, reinterpret_cast<unsigned __int64*>(&returnValue));
+	
+	std::cout << "THE CARRY BIT: " << +_addcarry_u64(0, inputB, inputA, reinterpret_cast<unsigned __int64*>(&returnValue)) << std::endl;
 	return returnValue;
 }
 
 class Simd8 {
   public:
 	Simd8(std::string& stringNewer) {
-		auto stringNew = reverseString(stringNewer);
+		auto stringNew = stringNewer;
 		std::cout << "THE ORIGINAL STRING: " << stringNewer << std::endl;
 		std::cout << "THE NEW STRING: " << stringNew << std::endl;
 		this->string = stringNew;
@@ -141,29 +151,37 @@ class Simd8 {
 		this->backslashes[1] = _mm256_set1_epi8('\\');
 		this->quotes[0] = _mm256_set1_epi8('"');
 		this->quotes[1] = _mm256_set1_epi8('"');
-		this->values[0] = _mm256_set_epi64x(*reinterpret_cast<int64_t*>(stringNew.data() + 24), *reinterpret_cast<int64_t*>(stringNew.data() + 16),
-			*reinterpret_cast<int64_t*>(stringNew.data() + 8), *reinterpret_cast<int64_t*>(stringNew.data() ));
-		this->values[1] = _mm256_set_epi64x(*reinterpret_cast<int64_t*>(stringNew.data() + 56), *reinterpret_cast<int64_t*>(stringNew.data() + 48),
-			*reinterpret_cast<int64_t*>(stringNew.data() + 40), *reinterpret_cast<int64_t*>(stringNew.data() + 32));
+		this->values[0] = packStringIntoValue(stringNewer.data());
+		this->values[1] = packStringIntoValue(stringNewer.data() + 32);
 		printValueAsString(this->values[0], "VALUES 0: ");
 		printValueAsString(this->values[1], "VALUES 1: ");
 		this->B[0] = _mm256_cmpeq_epi8(this->values[0], this->backslashes[0]);
 		this->B[1] = _mm256_cmpeq_epi8(this->values[1], this->backslashes[1]);
-		printValueAsString(convertTo32BitUint(this->B[0]), "B0 VALUES: ");
-		printValueAsString(convertTo32BitUint(this->B[1]), "B1 VALUES: ");
+		printValueAsString(this->B[0], "B0 VALUES: ");
+		printValueAsString(this->B[1], "B1 VALUES: ");
 		printValueAsString(convertTo64BitUint(this->B[0], this->B[1]), "TEST VALUES REAL: ");
-		
-		this->B64 = convertTo64BitUint(this->B[0], this->B[1]);
+		this->BShift = this->B64 >> 1;
+		this->B64 = convertTo64BitUint(this->B[1], this->B[0]);
 		printValueAsString(this->B64, "B64 VALUES: ");
-		printValueAsString(this->B64 >> 1, "B64-SHIFT VALUES: ");
-		this->S = this->B64 & ~(this->B64 >> 1);
+		printValueAsString(this->B64 << 1, "B64-SHIFT VALUES: ");
+		this->S = this->B64 & ~(this->B64 << 1);
 		printValueAsString(this->S, "S VALUES: ");
 		this->ES = this->S & this->E;
 		printValueAsString(this->ES, "ES VALUES: ");
-
+		printValueAsString(this->B64, "B64 VALUES: ");
 		//uint64_t odd_sequence_starts = this->B64 & ~;
 		this->EC = collectCarries(this->ES, this->B64);
+		uint64_t follows_escape = this->B64 >> 1;
 
+		// Get sequences starting on even bits by clearing out the odd series using +
+		const uint64_t even_bits = 0x5555555555555555ULL;
+		uint64_t odd_sequence_starts = this->B64 & ~even_bits & ~follows_escape;
+		auto sequences_starting_on_even_bits = collectCarries(odd_sequence_starts, this->B64);
+		uint64_t invert_mask = sequences_starting_on_even_bits << 1;// The mask we want to return is the *escaped* bits, not escapes.
+
+		// Mask every other backslashed character as an escaped character
+		// Flip the mask for sequences that start on even bits, to correct them
+		printValueAsString(even_bits ^ invert_mask, "THE VALUES: ");
 		//this->EC = collectCarries(this->ES, this->B64);
 		printValueAsString(this->EC, "EC VALUES: ");
 
@@ -179,8 +197,8 @@ class Simd8 {
 	__m256i quotes[2]{};
 	__m256i values[2]{};
 	__m256i B[2]{};
-	uint64_t E{ 0b1010101010101010101010101010101010101010101010101010101010101010 };
-	uint64_t O{ 0b0101010101010101010101010101010101010101010101010101010101010101 };
+	uint64_t E{ 0b0101010101010101010101010101010101010101010101010101010101010101 };
+	uint64_t O{ 0b1010101010101010101010101010101010101010101010101010101010101010 };
 	uint64_t B64{};
 	uint64_t BShift{};
 	uint64_t S{};
