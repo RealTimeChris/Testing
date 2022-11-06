136bd4f2a69cd481d065742e0e26a9ad25e091c8
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-11-06 09:28:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ void printValueAsString(__m256i in, std::string values) {
 void printValueAsString(uint64_t inA, std::string values) {
 	alignas(32) uint8_t v[64]{};
 	for (size_t x = 0; x < 64; ++x) {
-		if ((static_cast<uint64_t>(inA >> x) & 0x01) == 1 << 0) {
+		if ((static_cast<uint64_t>(inA >> x) & 0x01) == static_cast<uint64_t>(1 << 0)) {
 			v[x] = 1;
 		}
 	}
@@ -49,6 +49,28 @@ void printBits(__m256i value, std::string valuesTitle){
 	}
 	std::cout << std::endl;
 };
+
+std::string reverseString(std::string inputString) {
+	std::string newString{};
+	for (size_t x = 0; x < inputString.size(); ++x) {
+		newString.push_back(inputString[inputString.size() - x - 1]);
+	}
+	return newString;
+}
+
+__m256i packStringIntoValue(const char* string) {
+	__m256i value{};
+	for (size_t x = 0; x < 32; ++x) {
+		value.m256i_i8[x] = string[x];
+	}
+	return value;
+}
+
+uint64_t collectCarries(uint64_t inputA, uint64_t inputB) {
+	uint64_t returnValue{};
+	_addcarry_u64(0, inputB, inputA, reinterpret_cast<unsigned __int64*>(&returnValue));
+	return returnValue;
+}
 
 void printValueAsString(uint32_t inA, std::string values) {
 	alignas(32) uint8_t v[32]{};
@@ -97,11 +119,7 @@ struct Simd256Base {
 	}
 
 	inline Simd256Base(std::string& value) {
-		uint64_t value01 = convertStringToUint64(value.data());
-		uint64_t value02 = convertStringToUint64(value.data() + 8);
-		uint64_t value03 = convertStringToUint64(value.data() + 16);
-		uint64_t value04 = convertStringToUint64(value.data() + 24);
-		*this = { value01, value02, value03, value04 };
+		this->value = packStringIntoValue(value.data());
 		printBits(this->value, "THE TESTING VALUES 02: ");
 		__m256i backslashes[8]{};
 		__m256i StructuralTest[8]{};
@@ -127,7 +145,7 @@ struct Simd256Base {
 		StructuralTest[6] = _mm256_set1_epi8(0b00000111);
 		StructuralTest[7] = _mm256_set1_epi8(0b00000111);
 		quotes[0] = _mm256_set1_epi8('"');
-		quotes[1] = _mm256_set1_epi8('"');
+		quotes[1] = _mm256_set1_epi8('"'); /*
 		values[0] = packStringIntoValue(stringNewer.data());
 		values[1] = packStringIntoValue(stringNewer.data() + 32);
 		Structural[0] = _mm256_and_si256(this->values[0], this->StructuralTest[0]);
@@ -173,6 +191,7 @@ struct Simd256Base {
 		printValueAsString(whiteSpace01, "WHITESPACE VALUES 01: ");
 		auto whiteSpaceFinal = convertTo64BitUint(whiteSpace01, whiteSpace00);
 		printValueAsString(whiteSpaceFinal, "WHITESPACE FINAL VALUES: ");
+		*/
 	}
 
 	inline operator const __m256i&() const {
@@ -212,34 +231,10 @@ struct Simd256Base {
 	}
 };
 
-
-
-std::string reverseString(std::string inputString) {
-	std::string newString{};
-	for (size_t x = 0; x < inputString.size() ; ++x) {
-		newString.push_back(inputString[inputString.size() - x - 1]);
-	}
-	return newString;
-}
-
-__m256i packStringIntoValue(const char* string) {
-	__m256i value{};
-	for (size_t x = 0; x < 32; ++x) {
-		value.m256i_i8[x] = string[x];
-	}
-	return value;
-}
-
-uint64_t collectCarries(uint64_t inputA, uint64_t inputB) {
-	uint64_t returnValue{};
-	_addcarry_u64(0, inputB, inputA, reinterpret_cast<unsigned __int64*>(&returnValue));
-	return returnValue;
-}
-
 class Simd64Base {
   public:
 	Simd64Base(std::string& stringNewer) {
-		auto stringNew = stringNewer;
+		std::string& stringNew = stringNewer;
 		this->string = stringNew;
 		this->backslashes[0] = _mm256_set1_epi8('\\');
 		this->backslashes[1] = _mm256_set1_epi8('\\');
@@ -278,21 +273,27 @@ class Simd64Base {
 		this->R64 = _mm_cvtsi128_si64(_mm_clmulepi64_si128(_mm_set_epi64x(0ULL, this->R64), _mm_set1_epi8('\xFF'), 0));
 		printValueAsString(this->R64, "R VALUES: ");
 
-		const auto whitespaceTable = _mm256_setr_epi8(' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100, ' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t',
-			'\n', 112, 100, '\r', 100, 100);
+		const auto whitespaceTable = _mm256_setr_epi8(' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100, ' ', 100, 100, 100, 17, 100, 113, 2, 100,
+			'\t', '\n', 112, 100, '\r', 100, 100);
 		printValueAsString(whitespaceTable, "WHITESPACE TABLE: ");
-		//printValueAsString(this->values[0], "VALUES TABLE: ");
 		auto whiteSpace00 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(whitespaceTable, this->values[0]), this->values[0]);
 		auto whiteSpace01 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(whitespaceTable, this->values[1]), this->values[1]);
-		//auto whiteSpaceReal01 = _mm256_cmpeq_epi8(whiteSpace00, this->values[0]);
-		//printValueAsString(whiteSpaceReal01, "WHITESPACE 20");
-		//printValueAsString(_mm256_shuffle_epi8(whitespaceTable, this->values[0]), "WHITESPACE 00");
-		//printValueAsString(_mm256_shuffle_epi8(whitespaceTable, this->values[1]), "WHITESPACE 01");
-		printValueAsString(whiteSpace00, "WHITESPACE VALUES 01: ");
+		printValueAsString(whiteSpace00, "WHITESPACE VALUES 00: ");
 		printValueAsString(whiteSpace01, "WHITESPACE VALUES 01: ");
 		auto whiteSpaceFinal = convertTo64BitUint(whiteSpace01, whiteSpace00);
 		printValueAsString(whiteSpaceFinal, "WHITESPACE FINAL VALUES: ");
-		const auto opTable = _mm256_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0, 0);
+		const auto opTable = _mm256_setr_epi8(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0, 0);
+		printValueAsString(opTable, "STRUCTURAL TABLE: ");
+		auto valuesNew00 = _mm256_or_si256(_mm256_set1_epi8('0x20'), this->values[0]);
+		auto valuesNew01 = _mm256_or_si256(_mm256_set1_epi8('0x20'), this->values[1]);
+		printValueAsString(valuesNew00, "STRUCTURAL VALUES NEW 00: ");
+		printValueAsString(valuesNew01, "STRUCTURAL VALUES NEW 01: ");
+		auto structural00 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(opTable, this->values[0]), this->values[0]);
+		auto structural01 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(opTable, this->values[1]), this->values[1]);
+		printValueAsString(structural00, "STRUCTURAL VALUES 00: ");
+		printValueAsString(structural01, "STRUCTURAL VALUES 01: ");
+		auto structuralFinal = convertTo64BitUint(structural01, structural00);
+		printValueAsString(structuralFinal, "STRUCTURAL FINAL VALUES: ");
 
 
 	}
@@ -334,8 +335,20 @@ int32_t main() noexcept {
 	std::string string{ "{ \"\\\\\\\"Nam[{\": [ 116,\"\\\\\\\\\" , 234, \"true\", false ], \"t\":\"\\\\\\\"\" }" };
 	Simd64Base simd8Test{ string };
 	std::cout << "A VALUES:  (DIGITS) v64_u8: " << simd8Test.operator std::string() << std::endl;
+	Simd256Base value{ string };
 	uint64_t E{ 0b0101010101010101010101010101010101010101010101010101010101010101 };
 	uint64_t O{ 0b1010101010101010101010101010101010101010101010101010101010101010 };
-	Simd256Base value{ string };
+	int16_t valueNew{ static_cast<int16_t>(std::numeric_limits<int16_t>::max()) };
+	auto valueOld = valueNew;
+	valueNew += 1;
+	float valueNewer{ static_cast<float>(valueOld + 1) };
+	std::cout << "VALUE NEW-VALUE OLD: " << valueNew - valueOld << std::endl;
+	std::cout << "VALUE NEW: " << valueNew << std::endl;
+	std::cout << "VALUE OLD: " << valueOld << std::endl;
+
+	if (valueNew - valueOld < 0) {
+		std::cout << "WERE HERE THIS IS IT!" << std::endl;
+	}
+	
 	return 0;
 }
