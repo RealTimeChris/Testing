cc7689e807375a16a92c49b959388cb7feac8636
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-11-16 08:27:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -338,6 +338,10 @@ class SimdStringSection {
 		return this->RSB256;
 	}
 
+	SimdBase256& getBackslashesRange() {
+		return this->B256;
+	}
+
 	SimdBase256& getCommasRange() {
 		return this->C256;
 	}
@@ -443,16 +447,19 @@ class SimdStringSection {
 		return this->S256.bitAndNot(this->Q256.bitAndNot(this->R256));
 	}
 
-	SimdBase256 collectQuotes() {
+	SimdBase256 collectBackslashes() {
 		SimdBase256 backslashes = _mm256_set1_epi8('\\');
 		SimdBase256 backslashesReal[8]{};
 		for (size_t x = 0; x < 8; ++x) {
 			backslashesReal[x] = this->values[x] == backslashes;
 		}
 
-		auto B256 = SimdBase256{ convertSimd256To64BitUint(backslashesReal[0], backslashesReal[1]),
-			convertSimd256To64BitUint(backslashesReal[2], backslashesReal[3]), convertSimd256To64BitUint(backslashesReal[4], backslashesReal[5]),
-			convertSimd256To64BitUint(backslashesReal[6], backslashesReal[7]) };
+
+		return { convertSimd256To64BitUint(backslashesReal[0], backslashesReal[1]), convertSimd256To64BitUint(backslashesReal[2], backslashesReal[3]),
+			convertSimd256To64BitUint(backslashesReal[4], backslashesReal[5]), convertSimd256To64BitUint(backslashesReal[6], backslashesReal[7]) };
+	}
+
+	SimdBase256 collectQuotes() {
 		SimdBase256 quotes = _mm256_set1_epi8('"');
 		SimdBase256 quotesReal[8]{};
 		for (size_t x = 0; x < 8; ++x) {
@@ -461,16 +468,16 @@ class SimdStringSection {
 
 		this->Q256 = SimdBase256{ convertSimd256To64BitUint(quotesReal[0], quotesReal[1]), convertSimd256To64BitUint(quotesReal[2], quotesReal[3]),
 			convertSimd256To64BitUint(quotesReal[4], quotesReal[5]), convertSimd256To64BitUint(quotesReal[6], quotesReal[7]) };
-		auto S = B256.bitAndNot(B256 << 1);
+		auto S = this->B256.bitAndNot(this->B256 << 1);
 		SimdBase256 E{ _mm256_set1_epi8(0b01010101) };
 		SimdBase256 O{ _mm256_set1_epi8(0b10101010) };
 		auto ES = E & S;
-		auto EC = ES.collectCarries(B256);
-		auto ECE = EC.bitAndNot(B256);
+		auto EC = ES.collectCarries(this->B256);
+		auto ECE = EC.bitAndNot(this->B256);
 		auto OD1 = ECE.bitAndNot(E);
 		auto OS = S & O;
-		auto OC = B256 + OS;
-		auto OCE = OC.bitAndNot(B256);
+		auto OC = this->B256 + OS;
+		auto OCE = OC.bitAndNot(this->B256);
 		auto OD2 = OCE & E;
 		auto OD = OD1 | OD2;
 
@@ -496,6 +503,8 @@ class SimdStringSection {
 		this->packStringIntoValue(this->values[6], this->stringView.data() + 192);
 		this->packStringIntoValue(this->values[7], this->stringView.data() + 224);
 
+		this->B256 = this->collectBackslashes();
+
 		this->Q256 = this->collectQuotes();
 
 		//this->C256 = this->collectCommas();
@@ -515,10 +524,10 @@ class SimdStringSection {
 
 		this->S256 = collectStructuralCharacters();
 
-		//this->S256.printBits("S FINAL VALUES (256) ");
-		//this->W256.printBits("W FINAL VALUES (256) ");
-		//this->R256.printBits("R FINAL VALUES (256) ");
-		//this->Q256.printBits("Q FINAL VALUES (256): ");
+		this->S256.printBits("S FINAL VALUES (256) ");
+		this->W256.printBits("W FINAL VALUES (256) ");
+		this->R256.printBits("R FINAL VALUES (256) ");
+		this->Q256.printBits("Q FINAL VALUES (256): ");
 		//this->LSB256.printBits("LSB FINAL VALUES (256): ");
 		//this->RSB256.printBits("RSB FINAL VALUES (256) ");
 		//this->LCB256.printBits("LCB FINAL VALUES (256): ");
@@ -538,6 +547,7 @@ class SimdStringSection {
 	SimdBase256 Q256{};
 	SimdBase256 R256{};
 	SimdBase256 W256{};
+	SimdBase256 B256{};
 	SimdBase256 S256{};
 	SimdBase256 RSB256{};
 	SimdBase256 LSB256{};
