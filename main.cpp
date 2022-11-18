ec65933c01335afa3ef92258aab580cfb94d6be4
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-11-18 07:32:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -165,7 +165,7 @@ class SimdBase128 {
 	}
 
 	inline SimdBase128 shuffle(__m128i indices) {
-		return _mm_shuffle_epi8(indices, *this);
+		return _mm_shuffle_epi8(*this, indices);
 	}
 
 	inline void printBits(std::string valuesTitle) {
@@ -310,7 +310,7 @@ class SimdBase256 {
 	}
 
 	inline SimdBase256 shuffle(__m256i indices) {
-		return _mm256_shuffle_epi8(indices, *this);
+		return _mm256_shuffle_epi8(*this, indices);
 	}
 	
 	inline std::vector<int16_t> getSetBitIndices() {
@@ -406,7 +406,7 @@ class SimdStringSection {
 			100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100) };
 		SimdBase256 whiteSpaceReal[8]{};
 		for (size_t x = 0; x < 8; ++x) {
-			whiteSpaceReal[x] = this->values[x].shuffle(whitespaceTable) == this->values[x];
+			whiteSpaceReal[x] = whitespaceTable.shuffle(this->values[x]) == this->values[x];
 		}
 		return { convertSimd256To64BitUint(whiteSpaceReal[0], whiteSpaceReal[1]), convertSimd256To64BitUint(whiteSpaceReal[2], whiteSpaceReal[3]),
 			convertSimd256To64BitUint(whiteSpaceReal[4], whiteSpaceReal[5]), convertSimd256To64BitUint(whiteSpaceReal[6], whiteSpaceReal[7]) };
@@ -468,7 +468,7 @@ class SimdStringSection {
 		SimdBase256 structural[8]{};
 		for (size_t x = 0; x < 8; ++x) {
 			auto valuesNew00 = this->values[x] | SimdBase256{ 0x20 };
-			structural[x] = this->values[x].shuffle(opTable) == valuesNew00;
+			structural[x] = opTable.shuffle(this->values[x]) == valuesNew00;
 		}
 
 		this->S256 = SimdBase256{ convertSimd256To64BitUint(structural[0], structural[1]), convertSimd256To64BitUint(structural[2], structural[3]),
@@ -557,7 +557,7 @@ class SimdStringSection {
 		this->W256 = this->collectWhiteSpace();
 
 		this->S256 = this->collectStructuralCharacters();
-		//this->S256.printBits("S FINAL VALUES (256) ");
+		this->S256.printBits("S FINAL VALUES (256) ");
 		//this->W256.printBits("W FINAL VALUES (256) ");
 		//this->R256.printBits("R FINAL VALUES (256) ");
 		//this->Q256.printBits("Q FINAL VALUES (256): ");
@@ -639,8 +639,142 @@ class SimdStringScanner {
 	std::string finalString{};
 };
 
+class SimdBase64 {
+  public:
+	inline operator uint64_t() {
+		return convertSimd256To64BitUint(this->values[1], this->values[0]);
+	}
+
+	void packStringIntoValue(__m256i& theValue, const char* string) {
+		for (size_t x = 0; x < 32; ++x) {
+			*(reinterpret_cast<int8_t*>(&theValue) + x) = string[x];
+		}
+	}
+
+	void printBits(uint64_t inA, std::string values) {
+		alignas(32) uint8_t v[64]{};
+		for (size_t x = 0; x < 64; ++x) {
+			if ((static_cast<uint64_t>(inA >> x) & 0x01) == static_cast<uint64_t>(1 << 0)) {
+				v[x] = 1;
+			}
+		}
+		printf(std::string{ values.c_str() +
+				   std::string{ " (DIGITS) v64_u8: "
+								"%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%"
+								"d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n" } }
+				   .c_str(),
+			v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15], v[16], v[17], v[18], v[19], v[20],
+			v[21], v[22], v[23], v[24], v[25], v[26], v[27], v[28], v[29], v[30], v[31], v[32], v[33], v[34], v[35], v[36], v[37], v[38], v[39],
+			v[40], v[41], v[42], v[43], v[44], v[45], v[46], v[47], v[48], v[49], v[50], v[51], v[52], v[53], v[54], v[55], v[56], v[57], v[58],
+			v[59], v[60], v[61], v[62], v[63]);
+	}
+
+	uint64_t collectCarries(uint64_t inputA, uint64_t inputB) {
+		uint64_t returnValue{};
+		_addcarry_u64(0, inputB, inputA, reinterpret_cast<unsigned long long*>(&returnValue));
+		return returnValue;
+	}
+
+	void printBits(std::string valuesTitle) {
+		std::cout << valuesTitle;
+		for (size_t x = 0; x < 32; ++x) {
+			for (size_t y = 0; y < 8; ++y) {
+				//std::cout << std::bitset<1>{ reinterpret_cast<uint64_t*>(&values[0])[x] >> y };
+			}
+		}
+		for (size_t x = 0; x < 32; ++x) {
+			for (size_t y = 0; y < 8; ++y) {
+				//std::cout << std::bitset<1>{reinterpret_cast<uint64_t*>(&values[1])[x] >> y };
+			}
+		}
+		std::cout << std::endl;
+	};
+
+	inline SimdBase64(const __m256i& value01, __m256i value02) {
+		this->values[0] = value01;
+		this->values[1] = value02;
+	}
+
+	void collectQuotes() {
+		auto backslashes = _mm256_set1_epi8('\\');
+		auto B0 = _mm256_cmpeq_epi8(this->values[0], backslashes);
+		auto B1 = _mm256_cmpeq_epi8(this->values[1], backslashes);
+		auto B64 = convertSimd256To64BitUint(B0, B1);
+		this->S64 = B64 & ~(B64 << 1);
+		uint64_t E{ 0b0101010101010101010101010101010101010101010101010101010101010101 };
+		auto quotes = _mm256_set1_epi8('"');
+		uint64_t O{ 0b1010101010101010101010101010101010101010101010101010101010101010 };
+		auto ES = this->S64 & E;
+		auto EC = B64 + ES;
+		auto ECE = EC & ~B64;
+		auto OD1 = ECE & ~E;
+		auto OS = this->S64 & O;
+		auto OC = B64 + OS;
+		auto OCE = OC & ~B64;
+		auto OD2 = OCE & E;
+		auto OD = OD1 | OD2;
+		auto Q0 = _mm256_cmpeq_epi8(this->values[0], quotes);
+		auto Q1 = _mm256_cmpeq_epi8(this->values[1], quotes);
+		this->Q64 = convertSimd256To64BitUint(Q0, Q1);
+		this->Q64 &= ~OD;
+	}
+
+	void collectStructuralCharacters() {
+		__m256i opTable{ _mm256_setr_epi8(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{', ',', '}', 0,
+			0) };
+		this->R64 = this->Q64;
+		this->R64 = _mm_cvtsi128_si64(_mm_clmulepi64_si128(_mm_set_epi64x(0ULL, this->R64), _mm_set1_epi8('\xFF'), 0));
+		auto valuesNew00 = _mm256_or_si256(this->values[0], _mm256_set1_epi8(0x20));
+		auto valuesNew01 = _mm256_or_si256(this->values[1], _mm256_set1_epi8(0x20));
+		auto structural00 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(opTable, this->values[0]), valuesNew00);
+		auto structural01 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(opTable, this->values[1]), valuesNew01);
+		this->S64 = convertSimd256To64BitUint(structural00, structural01);
+		this->S64 = this->S64 & ~this->R64;
+		this->S64 = this->S64 | this->Q64;
+		auto P64 = this->S64 | this->W64;
+		P64 = P64 << 1;
+		P64 &= ~this->W64 & ~this->R64;
+		this->S64 = this->S64 | P64;
+		this->S64 = this->S64 & ~(this->Q64 & ~this->R64);
+	}
+
+	void collectWhiteSpace() {
+		__m256i whitespaceTable{ _mm256_setr_epi8(' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100, ' ', 100, 100, 100,
+			17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100) };
+		auto whiteSpace00 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(whitespaceTable, this->values[0]), this->values[0]);
+		auto whiteSpace01 = _mm256_cmpeq_epi8(_mm256_shuffle_epi8(whitespaceTable, this->values[1]), this->values[1]);
+		this->W64 = convertSimd256To64BitUint(whiteSpace00, whiteSpace01);
+	}
+
+	inline SimdBase64(std::string& stringNewer) {
+		this->string = stringNewer;
+		packStringIntoValue(this->values[0], stringNewer.data());
+		packStringIntoValue(this->values[1], stringNewer.data() + 32);
+		this->collectQuotes();
+		this->collectWhiteSpace();
+		this->collectStructuralCharacters();
+
+		//printBits(this->Q64, "Q FINAL VALUES: ");
+		//printBits(this->R64, "R FINAL VALUES: ");
+		//printBits(this->S64, "S FINAL VALUES: ");
+		//printBits(this->W64, "W FINAL VALUES: ");
+	}
+
+	operator std::string() {
+		return string;
+	}
+
+  protected:
+	__m256i values[2]{};
+	std::string string{};
+	uint64_t Q64{};
+	uint64_t R64{};
+	uint64_t S64{};
+	uint64_t W64{};
+};
+
 int32_t main() noexcept {
-	
+	std::cout << sizeof(std::vector<char>) << sizeof(std::string) << std::endl;
 	std::string string64{ "{ \"\\\\\\\"Nam[{\": [ 116,\"\\\\\\\\\" , 234, \"true\", false ], \"t\":\"\\\\\\\"\" }" };
 	std::string string256{ "{ \"\\\\\\\"Nam[{\": [ 116,\"\\\\\\\\\" , 234, \"true\", false ], \"t\":\"\\\\\\\"\" }"
 						   "{ \"\\\\\\\"Nam[{\": [ 116,\"\\\\\\\\\" , 234, \"true\", false ], \"t\":\"\\\\\\\"\" }"
@@ -668,7 +802,7 @@ int32_t main() noexcept {
 	totalTime=totalTime.zero();
 	stopWatch.resetTimer();
 	for (size_t x = 0; x < 256 * 16384; ++x) {
-		//SimdBase64 simd8Test{ string64 };
+		SimdBase64 simd8Test{ string64 };
 		totalSize += string64.size();
 	}
 	totalTime += stopWatch.totalTimePassed();
