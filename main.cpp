d73d552ea2f2decf19a12de8332bb9816b88fc57
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-12-17 22:35:56 -0500

    Updates!

--- main.cpp
+++ main.cpp
@@ -1,19 +1,264 @@
 #include "jsonifier/Jsonifier.hpp"
-#include "DataParsingFunctionc.hpp"
 #include <simdjson.h>
+
+struct ObjectReturnData {
+	simdjson::ondemand::value object{};
+	bool didItSucceed{ false };
+};
+
+struct ArrayReturnData {
+	simdjson::ondemand::array arrayValue{};
+	bool didItSucceed{ false };
+};
+
+int64_t getInt64(simdjson::ondemand::value jsonData, const char* key);
+
+int32_t getInt32(simdjson::ondemand::value jsonData, const char* key);
+
+int16_t getInt16(simdjson::ondemand::value jsonData, const char* key);
+
+int8_t getInt8(simdjson::ondemand::value jsonData, const char* key);
+
+uint64_t getUint64(simdjson::ondemand::value jsonData, const char* key);
+
+uint32_t getUint32(simdjson::ondemand::value jsonData, const char* key);
+
+uint16_t getUint16(simdjson::ondemand::value jsonData, const char* key);
+
+uint8_t getUint8(simdjson::ondemand::value jsonData, const char* key);
+
+float getFloat(simdjson::ondemand::value jsonData, const char* key);
+
+bool getBool(simdjson::ondemand::value jsonData, const char* key);
+
+std::string getString(simdjson::ondemand::value jsonData, const char* key);
+
+std::string getString(ObjectReturnData jsonData, const char* key);
+
+bool getObject(simdjson::ondemand::value& object, const char* key, simdjson::ondemand::value jsonObjectData);
+
+bool getArray(simdjson::ondemand::array& array, const char* key, simdjson::ondemand::value jsonObjectData);
+
+bool getArray(simdjson::ondemand::array& array, simdjson::ondemand::value jsonObjectData);
+
+std::string getString(simdjson::ondemand::value jsonData);
+
+ObjectReturnData getObject(simdjson::ondemand::value jsonObjectData, const char* objectName);
+
+ObjectReturnData getObject(ObjectReturnData jsonObjectData, const char* objectName);
+
+ObjectReturnData getObject(ArrayReturnData jsonObjectData, uint64_t objectIndex);
+
+ArrayReturnData getArray(simdjson::ondemand::value jsonObjectData, const char* arrayName);
+
+ArrayReturnData getArray(ObjectReturnData jsonObjectData, const char* arrayName);
+
+	int64_t getInt64(simdjson::ondemand::value jsonData, const char* key) {
+	int64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return int64_t{ value };
+	} else {
+		return 0;
+	}
+}
+
+int32_t getInt32(simdjson::ondemand::value jsonData, const char* key) {
+	int64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<int32_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+int16_t getInt16(simdjson::ondemand::value jsonData, const char* key) {
+	int64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<int16_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+int8_t getInt8(simdjson::ondemand::value jsonData, const char* key) {
+	int64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<int8_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+uint64_t getUint64(simdjson::ondemand::value jsonData, const char* key) {
+	uint64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return uint64_t{ value };
+	} else {
+		return 0;
+	}
+}
+
+uint32_t getUint32(simdjson::ondemand::value jsonData, const char* key) {
+	uint64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<uint32_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+uint16_t getUint16(simdjson::ondemand::value jsonData, const char* key) {
+	uint64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<uint16_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+uint8_t getUint8(simdjson::ondemand::value jsonData, const char* key) {
+	uint64_t value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<uint8_t>(value);
+	} else {
+		return 0;
+	}
+}
+
+float getFloat(simdjson::ondemand::value jsonData, const char* key) {
+	double value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return double{ value };
+	} else {
+		return 0.0f;
+	}
+}
+
+bool getBool(simdjson::ondemand::value jsonData, const char* key) {
+	bool value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return bool{ value };
+	} else {
+		return false;
+	}
+}
+
+std::string getString(simdjson::ondemand::value jsonData, const char* key) {
+	std::string_view value{};
+	if (jsonData[key].get(value) == simdjson::error_code::SUCCESS) {
+		return std::string{ value.data(), value.size() };
+	} else {
+		return "";
+	}
+}
+
+bool getObject(simdjson::ondemand::value& object, const char* key, simdjson::ondemand::value jsonObjectData) {
+	if (jsonObjectData[key].get(object) == simdjson::error_code::SUCCESS) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool getArray(simdjson::ondemand::array& array, const char* key, simdjson::ondemand::value jsonObjectData) {
+	if (jsonObjectData[key].get(array) == simdjson::error_code::SUCCESS) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool getArray(simdjson::ondemand::array& array, simdjson::ondemand::value jsonObjectData) {
+	if (jsonObjectData.get(array) == simdjson::error_code::SUCCESS) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+std::string getString(ObjectReturnData jsonData, const char* key) {
+	std::string_view value{};
+	if (jsonData.didItSucceed && jsonData.object[key].get(value) == simdjson::error_code::SUCCESS) {
+		return static_cast<std::string>(value);
+	}
+	return static_cast<std::string>(value);
+}
+
+std::string getString(simdjson::ondemand::value jsonData) {
+	std::string_view value{};
+	if (jsonData.get(value) == simdjson::error_code::SUCCESS) {
+		return std::string{ value.data(), value.size() };
+	} else {
+		return "";
+	}
+}
+
+ObjectReturnData getObject(simdjson::ondemand::value jsonData, const char* objectName) {
+	ObjectReturnData value{};
+	if (jsonData[objectName].get(value.object) == simdjson::error_code::SUCCESS) {
+		value.didItSucceed = true;
+	}
+	return value;
+}
+
+ObjectReturnData getObject(ObjectReturnData jsonData, const char* objectName) {
+	ObjectReturnData value{};
+	if (jsonData.didItSucceed && jsonData.object[objectName].get(value.object) == simdjson::error_code::SUCCESS) {
+		value.didItSucceed = true;
+	}
+	return value;
+}
+
+ObjectReturnData getObject(ArrayReturnData jsonData, uint64_t objectIndex) {
+	ObjectReturnData value{};
+	if (jsonData.didItSucceed && jsonData.arrayValue.at(objectIndex).get(value.object) == simdjson::error_code::SUCCESS) {
+		value.didItSucceed = true;
+	}
+	return value;
+}
+
+ArrayReturnData getArray(simdjson::ondemand::value jsonData, const char* arrayName) {
+	ArrayReturnData value{};
+	if (jsonData[arrayName].get(value.arrayValue) == simdjson::error_code::SUCCESS) {
+		value.didItSucceed = true;
+	}
+	return value;
+}
+
+ArrayReturnData getArray(ObjectReturnData jsonData, const char* arrayName) {
+	ArrayReturnData value{};
+	if (jsonData.didItSucceed && jsonData.object[arrayName].get(value.arrayValue) == simdjson::error_code::SUCCESS) {
+		value.didItSucceed = true;
+	}
+	return value;
+}
+
 
 struct ActivitiesJson {
 	ActivitiesJson() noexcept = default;
 	ActivitiesJson(Jsonifier::Jsonifier&& value) {
 		this->createdAt = value["created_at"].getValue<std::string>();
+		this->name = value["name"].getValue<std::string>();
+		this->anotherValue = value["ANOTHER_VALUE"].getValue<int32_t>();
+		this->anotherTestValue = value["ANOTHER_TEST_VALUE"].getValue<std::string>();
+		this->anotherValue02 = value["ANOTHER_VALUE_02"].getValue<int32_t>();
 		this->id = value["id"].getValue<std::string>();
+		this->type = value["type"].getValue<int8_t>();
 		this->testDouble = value["test_double"].getValue<double>();
 	};
 	std::string createdAt{};
 	double testDouble{};
+	int8_t type{};
+	std::string name{};
+	int32_t anotherValue{};
+	std::string anotherTestValue{};
+	int32_t anotherValue02{};
+	std::string anotherTestValue03{};
+	std::string anotherTestValue03d{};
+	int32_t anotherValue02w{};
 	std::string id{};
-	operator Jsonifier::JsonSerializer() {
-		Jsonifier::JsonSerializer data{};
+	operator Jsonifier::Jsonifier() {
+		Jsonifier::Jsonifier data{};
 		data["test_double"] = this->testDouble;
 		data["created_at"] = this->createdAt;
 		data["id"] = this->id;
@@ -29,8 +274,8 @@ struct TheDJson {
 			activities.emplace_back(std::move(value));
 		}
 	}
-	operator Jsonifier::JsonSerializer() {
-		Jsonifier::JsonSerializer serializer{};
+	operator Jsonifier::Jsonifier() {
+		Jsonifier::Jsonifier serializer{};
 		for (auto& value: this->activities) {
 			serializer["d"]["activitiess"].emplaceBack(value);
 		}
@@ -55,12 +300,25 @@ struct TheValueJson {
 
 struct Activities {
 	Activities(simdjson::ondemand::value value) {
-		this->createdAt = DiscordCoreAPI::getString(value, "created_at");
-		this->id = DiscordCoreAPI::getString(value, "id");
-		this->testDouble = DiscordCoreAPI::getFloat(value, "test_double");
+		this->createdAt = getString(value, "created_at");
+		this->name = getString(value, "name");
+		this->anotherValue = getInt32(value, "ANOTHER_VALUE");
+		this->anotherTestValue = getString(value, "ANOTHER_TEST_VALUE");
+		this->anotherValue02 = getInt32(value, "ANOTHER_VALUE_02");
+		this->id = getString(value, "id");
+		this->type = getUint8(value, "type");
+		this->testDouble = getFloat(value, "test_double");
 	};
 	std::string createdAt{};
 	double testDouble{};
+	int8_t type{};
+	std::string name{};
+	std::string anotherValue{};
+	std::string anotherTestValue{};
+	int32_t anotherValue02{};
+	std::string anotherTestValue03{};
+	std::string anotherTestValue03d{};
+	int32_t anotherValue02w{};
 	std::string id{};
 };
 
@@ -69,7 +327,7 @@ struct TheD {
 	TheD(simdjson::ondemand::value value) {
 		simdjson::ondemand::value valueNew{};
 		value["d"]["activitiess"].get(valueNew);
-		auto theArray = DiscordCoreAPI::getArray(value["d"], "activitiess");
+		auto theArray = getArray(value["d"], "activitiess");
 		if (theArray.didItSucceed) {
 			for (auto value: theArray.arrayValue) {
 				activities.push_back(value.value());
@@ -98,20 +356,11 @@ int32_t main() noexcept {
 		size_t totalTime{};
 		size_t totalSize{};
 		size_t oldSize = stringNew.size();
-		stopWatch.resetTimer();
-		Jsonifier::StringPackage stringPackage{ Jsonifier::prepStringForParsing(stringNew) };
-		for (size_t x = 0ull; x < 2048ull * 64ull; ++x) {
-			Jsonifier::Jsonifier jsonData{};
-			jsonData.parseString(stringPackage);
-			TheValueJson value{ jsonData };
-			totalSize += oldSize;
-		}
-		totalTime += stopWatch.totalTimePassed().count();
+		
 
-		std::cout << "IT TOOK: " << totalTime << "ns TO PARSE THROUGH IT: " << totalSize << " BYTES!" << std::endl;
+		
 		std::string stringNewer = stringNew;
-		totalSize = 0;
-		totalTime = 0;
+		
 		stopWatch.resetTimer();
 		stringNewer.reserve(stringNewer.size() + simdjson::SIMDJSON_PADDING);
 		for (size_t x = 0ull; x < 2048ull * 64ull; ++x) {
@@ -120,7 +369,21 @@ int32_t main() noexcept {
 			TheValue value{ newDocument };
 			totalSize += oldSize;
 		}
+		totalTime += stopWatch.totalTimePassed().count();
+		std::cout << "IT TOOK: " << totalTime << "ns TO PARSE THROUGH IT: " << totalSize << " BYTES!" << std::endl;
 
+		totalSize = 0;
+		totalTime = 0;
+		stopWatch.resetTimer();
+		Jsonifier::StringPackage stringPackage{ Jsonifier::prepStringForParsing(stringNew) };
+		for (size_t x = 0ull; x < 2048ull * 64ull; ++x) {
+			Jsonifier::Jsonifier jsonData{};
+			jsonData.parseString(stringPackage);
+			TheValueJson value{ jsonData };
+			//std::cout << "THE VALUE: " << value.theD.activities.back().testDouble << std::endl;
+			//std::cout << "THE VALUE: " << value.theD.activities.back().anotherTestValue << std::endl;
+			totalSize += oldSize;
+		}
 		totalTime += stopWatch.totalTimePassed().count();
 		std::cout << "IT TOOK: " << totalTime << "ns TO PARSE THROUGH IT: " << totalSize << " BYTES!" << std::endl;
 
