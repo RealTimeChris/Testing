9027f34159e8d39718411b6623cfebfe1e7d9455
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-12-26 07:19:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -132,7 +132,7 @@ int32_t main() noexcept {
 		arrayValueNew["TEST_VALUE_11"] = 4325454;
 		auto arrayValue = arrayValueNew;
 		//arrayValueNew["TEST_VALUE_95"] = arrayValue;
-		for (size_t x = 0; x < 5; ++x) {
+		for (size_t x = 0; x < 4; ++x) {
 			serializer["d"]["activitiess_TEST"] = false;
 			serializer["d"]["activitiess_TEST02"] = 0.0342;
 			serializer["d"]["activitiess_TEST03"] = 342;
