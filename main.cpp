02a8d39f311340c110c8032a4656b6449391c116
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2023-01-02 10:41:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ struct TheDJson {
 		//	std::string_view newValueDouble = iter->get<std::string_view>().getValue();
 			//std::cout << "NEW INDEX: " << newValueDouble << std::endl;
 			//}
-		//std::cout << "NEW INDEX: WERE DONE" << index << std::endl;
+		std::cout << "NEW INDEX: WERE DONE" << std::endl;
 		iterationCount = 0;
 		totalTime = 0;
 		stopWatch.resetTimer();
